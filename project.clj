1de54c5f66b592e7d1719eca8308741ad1543fa0
Florin Patan <florinpatan@gmail.com>
2017-05-15 13:51:09 +0100

    Upgrade dependencies

--- project.clj
+++ project.clj
@@ -4,13 +4,13 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
-                 [ring/ring-core "1.4.0"]
-                 [com.amazonaws/aws-xray-recorder-sdk-core "1.0.0-beta"]
-                 [com.amazonaws/aws-xray-recorder-sdk-aws-sdk "1.0.0-beta"]
-                 [com.amazonaws/aws-xray-recorder-sdk-aws-sdk-instrumentor "1.0.0-beta"]
-                 [com.amazonaws/aws-xray-recorder-sdk-apache-http "1.0.0-beta"]
-                 [com.amazonaws/aws-xray-recorder-sdk-sql-postgres "1.0.0-beta"]
-                 [com.amazonaws/aws-java-sdk-xray "1.11.64"]]
+                 [ring/ring-core "1.6.1"]
+                 [com.amazonaws/aws-xray-recorder-sdk-core "1.1.0"]
+                 [com.amazonaws/aws-xray-recorder-sdk-aws-sdk "1.1.0"]
+                 [com.amazonaws/aws-xray-recorder-sdk-aws-sdk-instrumentor "1.1.0"]
+                 [com.amazonaws/aws-xray-recorder-sdk-apache-http "1.1.0"]
+                 [com.amazonaws/aws-xray-recorder-sdk-sql-postgres "1.1.0"]
+                 [com.amazonaws/aws-java-sdk-xray "1.11.128"]]
   :aliases {"test-all" ["with-profile" "default:+1.8:+1.9" "test"]}
   :profiles
   {:dev {:dependencies [[ring/ring-mock "0.3.0"]
