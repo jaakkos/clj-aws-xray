6e0052aa175a0f2f54f9a4850de29f061623a769
Jaakko Suutarla <jaakko@suutarla.com>
2017-05-22 08:45:09 +0300

    Upgrade dependencies

--- project.clj
+++ project.clj
@@ -5,16 +5,16 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [ring/ring-core "1.6.1"]
-                 [com.amazonaws/aws-xray-recorder-sdk-core "1.1.0"]
-                 [com.amazonaws/aws-xray-recorder-sdk-aws-sdk "1.1.0"]
-                 [com.amazonaws/aws-xray-recorder-sdk-aws-sdk-instrumentor "1.1.0"]
-                 [com.amazonaws/aws-xray-recorder-sdk-apache-http "1.1.0"]
-                 [com.amazonaws/aws-xray-recorder-sdk-sql-postgres "1.1.0"]
-                 [com.amazonaws/aws-java-sdk-xray "1.11.128"]]
+                 [com.amazonaws/aws-xray-recorder-sdk-core "1.1.1"]
+                 [com.amazonaws/aws-xray-recorder-sdk-aws-sdk "1.1.1"]
+                 [com.amazonaws/aws-xray-recorder-sdk-aws-sdk-instrumentor "1.1.1"]
+                 [com.amazonaws/aws-xray-recorder-sdk-apache-http "1.1.1"]
+                 [com.amazonaws/aws-xray-recorder-sdk-sql-postgres "1.1.1"]
+                 [com.amazonaws/aws-java-sdk-xray "1.11.132"]]
   :aliases {"test-all" ["with-profile" "default:+1.8:+1.9" "test"]}
   :profiles
   {:dev {:dependencies [[ring/ring-mock "0.3.0"]
-                        [org.mockito/mockito-core "2.3.0"]]}
+                        [org.mockito/mockito-core "2.8.9"]]}
    :1.8 {:dependencies [[org.clojure/clojure "1.8.0"]]}
    :1.9 {:dependencies [[org.clojure/clojure "1.9.0-alpha10"]]}}
   :deploy-repositories
